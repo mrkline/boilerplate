3c682f908aca7bbd0fa7e2049547bb35acbd508e
Matt Kline <mrkline@wisc.edu>
2011-10-23 23:54:34 -0500

    Removed About menu option until it's implemented

--- frmMain.Designer.cs
+++ frmMain.Designer.cs
@@ -33,7 +33,6 @@
 			this.menu = new System.Windows.Forms.MenuStrip();
 			this.tsmFile = new System.Windows.Forms.ToolStripMenuItem();
 			this.tsiOpen = new System.Windows.Forms.ToolStripMenuItem();
-			this.tsmAbout = new System.Windows.Forms.ToolStripMenuItem();
 			this.dlgOpen = new System.Windows.Forms.OpenFileDialog();
 			this.pnlFields = new System.Windows.Forms.Panel();
 			this.lblFieldsWillAppear = new System.Windows.Forms.Label();
@@ -57,8 +56,7 @@
 			// 
 			this.menu.BackColor = System.Drawing.SystemColors.Control;
 			this.menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.tsmFile,
-            this.tsmAbout});
+            this.tsmFile});
 			this.menu.Location = new System.Drawing.Point(0, 0);
 			this.menu.Name = "menu";
 			this.menu.Size = new System.Drawing.Size(284, 24);
@@ -79,15 +77,9 @@
 			this.tsiOpen.ImageTransparentColor = System.Drawing.Color.Magenta;
 			this.tsiOpen.Name = "tsiOpen";
 			this.tsiOpen.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
-			this.tsiOpen.Size = new System.Drawing.Size(146, 22);
+			this.tsiOpen.Size = new System.Drawing.Size(152, 22);
 			this.tsiOpen.Text = "&Open";
 			this.tsiOpen.Click += new System.EventHandler(this.tsiOpen_Click);
-			// 
-			// tsmAbout
-			// 
-			this.tsmAbout.Name = "tsmAbout";
-			this.tsmAbout.Size = new System.Drawing.Size(52, 20);
-			this.tsmAbout.Text = "&About";
 			// 
 			// dlgOpen
 			// 
@@ -140,7 +132,6 @@
 		private System.Windows.Forms.MenuStrip menu;
 		private System.Windows.Forms.ToolStripMenuItem tsmFile;
 		private System.Windows.Forms.ToolStripMenuItem tsiOpen;
-		private System.Windows.Forms.ToolStripMenuItem tsmAbout;
 		private System.Windows.Forms.OpenFileDialog dlgOpen;
 		private System.Windows.Forms.Panel pnlFields;
 		private System.Windows.Forms.Label lblFieldsWillAppear;
